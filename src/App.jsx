2d039f9086b0fef267ea2d2daf687809fc05ceba
Kritsada Tariya <mr.kritsada.t@gmail.com>
2024-04-17 22:58:17 +0700

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      <h1>ReactJs Unit Test App + Vitest</h1>
+      <div className="card">
+        <Counter />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Counter is : {count}</h1>
+      <div className="container-button">
+        <button
+          className="button-increment"
+          onClick={() => setCount(count + 1)}
+        >
+          Increment
+        </button>
+        <button
+          className="button-decrement"
+          onClick={() => setCount(count - 1)}
+          disabled={count <= 0}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
